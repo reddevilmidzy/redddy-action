63e3da06b8f9c2eb45aa318d69f80c2d86ad7edd
reddevilmidzy <midzy0228@gmail.com>
2024-08-04 15:44:28 +0900

    feat: A + B 코드 추가

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,20 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
 
-    public static void main(final String[] args) {
-        System.out.println("알유레디");
+    public static void main(final String[] args) throws IOException {
+
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final StringTokenizer st = new StringTokenizer(br.readLine());
+
+        final int a = Integer.parseInt(st.nextToken());
+        final int b = Integer.parseInt(st.nextToken());
+
+        System.out.println(a + b);
     }
 }
